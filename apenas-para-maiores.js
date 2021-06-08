cabe5dee0118a631afe3fd5594d6a75f9df9db67
Natália Barbosa <nob.ufba@gmail.com>
2021-06-08 19:18:56 -0300

    HackerRank execício Apenas para maiores

---
+++ apenas-para-maiores.js
@@ -0,0 +1,27 @@
+
+
+function solucao(idade) {
+    //seu codigo aqui
+      if(idade >= 18){
+          console.log('PODE ENTRAR');
+      }
+      else{
+          console.log('ACESSO NEGADO');
+      }
+    
+  }
+  
+  function processData(input) {
+        solucao(parseInt(input.trim(), 10));
+  } 
+  
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  _input = "";
+  process.stdin.on("data", function (input) {
+      _input += input;
+  });
+  
+  process.stdin.on("end", function () {
+     processData(_input);
+  });
