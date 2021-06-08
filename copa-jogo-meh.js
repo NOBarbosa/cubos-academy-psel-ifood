addfde17cbf4fe4aa36c0e7413da09709e4bb743
Natália Barbosa <nob.ufba@gmail.com>
2021-06-08 19:25:13 -0300

    HackerRank Tem copa? Tem jogos? Não? MEH

---
+++ copa-jogo-meh.js
@@ -0,0 +1,32 @@
+
+
+function solucao(ano) {
+	//seu codigo aqui
+    
+    if(ano % 2 == 0 && ano % 4 == 0){
+        console.log('JOGOS')
+    }
+    else if(ano % 2 == 0){
+        console.log('COPA')
+    }
+    else{
+        console.log('MEH')
+    }
+    
+    
+}
+
+function processData(input) {
+	solucao(parseInt(input, 10));
+} 
+
+process.stdin.resume();
+process.stdin.setEncoding("ascii");
+_input = "";
+process.stdin.on("data", function (input) {
+    _input += input;
+});
+
+process.stdin.on("end", function () {
+   processData(_input);
+});
