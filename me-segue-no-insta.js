dd3ae2b7b6786d275da3344f52b2700e2be40a83
Natália Barbosa <nob.ufba@gmail.com>
2021-06-08 19:28:25 -0300

    HackerRank Me segue no Insta

---
+++ me-segue-no-insta.js
@@ -0,0 +1,30 @@
+
+
+function solucao(largura, altura) {
+    //seu codigo aqui
+    if(altura > largura){
+        console.log('RETRATO');
+    }
+    else if(altura < largura){
+        console.log('PAISAGEM');
+    }  
+      else{
+          console.log('QUADRADA');
+      }
+  }
+  
+  function processData(input) {
+      const x = input.trim().split(" ");
+        solucao(parseInt(x[0], 10), parseInt(x[1], 10));
+  } 
+  
+  process.stdin.resume();
+  process.stdin.setEncoding("ascii");
+  _input = "";
+  process.stdin.on("data", function (input) {
+      _input += input;
+  });
+  
+  process.stdin.on("end", function () {
+     processData(_input);
+  });
